74245bd0fe146c1d414247664fd0e76673d5ae13
Trevor Irons <tirons@egi.utah.edu>
2017-06-09 10:39:17 -0600

    First commit to ReGrid, a grid conversion utility

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, Extension
+
+setup(name='ReGrid',
+      version='0.0.1',
+      description='Grid conversion utilities for multiphase flow modelling software',
+      author='Trevor P. Irons',
+      author_email='Trevor.Irons@egi.utah.edu',
+      url='https://git.lemmasofware.org/co2/ReGrid',
+      setup_requires=[
+        'setuptools>=18.0',
+      ],
+      build_requires=['cython'],
+      install_requires=[
+          'matplotlib',
+          'scipy',
+          'numpy',
+          'cmocean'
+      ],
+      packages=['regrid', 'regrid.flowgrid'],
+      license=['GPL 4.0'],
+)
